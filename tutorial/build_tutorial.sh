8aa10b09e396cac0c8fb26e07c5a0a6f430e691f
Eyal Hochberg <eyalhoc@gmail.com>
2025-06-23 10:58:28 -0400

    allow cache on empty dir

--- tutorial/build_tutorial.sh
+++ tutorial/build_tutorial.sh
@@ -7,8 +7,8 @@ SCRIPT_DIR=`dirname $0`
 for d in `ls $SCRIPT_DIR`
 do
     if [ -d $SCRIPT_DIR/$d ];then
-	if [ -e $SCRIPT_DIR/$d/run.sh ];then
-	    bash $SCRIPT_DIR/$d/run.sh
+	if [ -e $SCRIPT_DIR/$d/build.sh ];then
+	    bash $SCRIPT_DIR/$d/build.sh
             if [ $? -gt 0 ];then
 	        exit 1
             fi
---
+++ tutorial/example0_hello_world/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+SCRIPT_DIR=`dirname $0`
+
+OUTDIR=$SCRIPT_DIR/results
+rm -rf $OUTDIR
+mkdir $OUTDIR
+
+python3 $SCRIPT_DIR/hello_world.py -outdir $OUTDIR -allow_missing_tools
+if [ $? -gt 0 ];then
+	exit 1
+fi
+
+echo "completed successfully"
