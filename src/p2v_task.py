89576f3cc4c58904d10baf494c392bec624bef5e
Eyal Hochberg <eyalhoc@gmail.com>
2025-12-08 14:37:41 -0500

    added commnets to task functions

--- src/p2v_task.py
+++ src/p2v_task.py
@@ -29,15 +29,19 @@ class p2v_task():
         self._p2v._base_depth += 1
 
     def input(self, name="", bits=1):
+        """ task input """
         return self._p2v.input(name=name, bits=bits)
 
     def output(self, name="", bits=1):
+        """ task output """
         return self._p2v.output(name=name, bits=bits)
 
     def assign(self, tgt, src, _allow_str=False):
+        """ task assignment """
         return self._p2v.assign(tgt, src, keyword="", _allow_str=_allow_str)
 
     def write(self, automatic=True):
+        """ end task """
         task_name = self.__class__.__name__
         ports = []
         lines = []
@@ -66,38 +70,28 @@ class p2v_task():
         return func
 
     def line(self, line="", remark=None):
+        """ add line to task """
         return self._p2v.line(line, remark=remark)
 
     def logic(self, name="", bits=1, assign=None, _allow_str=False):
+        """ define task varibale (reg) """
         return self._p2v.logic(name=name, bits=bits, assign=assign, _task=True, _allow_str=_allow_str)
 
-    def get_data(self, fd, bits=8):
-        _data = self._parent._get_receive_name("get_data")
-        _status = self.logic(f"_status_{_data}", 32, _allow_str=True)
-        _line = self.logic(f"_line_{_data}", bits*8, _allow_str=True) # 8 bits for char
-        _data = self.logic(bits)
-        self.assign(_status, f"$fgets({_line}, {fd})", _allow_str=True)
-        self.assign(_status, f'$sscanf({_line}, "%x", {_data})', _allow_str=True)
-        return _data
-
-    def fopen(self, filename):
-        fd = self.logic(32, assign=f'$fopen({filename}, "r")', _allow_str=True)
-        return fd
-
-    def fclose(self, fd):
-        self.line(f"$fclose({fd});")
-
     def loop(self, size, start_idx=0):
+        """ start for loop in task body """
         name = self._parent._get_receive_name("loop")
         return self._p2v.tb.loop(size, start_idx=start_idx, name=name, _task=True)
 
     def end(self):
+        """ end loop """
         return self._p2v.tb.end()
 
     def syn_off(self):
+        """ translate off """
         return self._p2v.tb.syn_off()
 
     def syn_on(self):
+        """ translate on """
         return self._p2v.tb.syn_on()
 
     def assert_property(self, condition=None, message=None, name=None, valid=None, fatal=True, clk=None):
@@ -105,19 +99,42 @@ class p2v_task():
                                          name=name, valid=valid, fatal=fatal, concurrent=False)
 
     def display(self, s, params=None, cond=None):
+        """ Verilog display """
         return self._p2v.tb.display(s, params=params, cond=cond)
 
     def delay(self, signal, num=1, posedge=True, wait_for=None):
+        """ wait fixed number of cycles or wait for event """
         return self._p2v.tb.delay(signal, num=num, posedge=posedge, wait_for=wait_for)
 
     def exec(self, func, cond=None):
+        """ execute verilog task """
         return self._p2v.exec(func, cond=cond)
 
     def readmemh(self, filename, dim):
+        """ read hex file into memory (2d reg) """
         assert len(dim) == 2, "readmemh memory must be 2d"
         readmemh_array = self.logic(dim)
         self.line(f"$readmemh({filename}, {readmemh_array});")
         return readmemh_array
+
+    def fopen(self, filename):
+        """ open file """
+        fd = self.logic(32, assign=f'$fopen({filename}, "r")', _allow_str=True)
+        return fd
+
+    def fclose(self, fd):
+        """ close file """
+        self.line(f"$fclose({fd});")
+
+    def get_data(self, fd, bits=8):
+        """ get data from file """
+        _data = self._parent._get_receive_name("get_data")
+        _status = self.logic(f"_status_{_data}", 32, _allow_str=True)
+        _line = self.logic(f"_line_{_data}", bits*8, _allow_str=True) # 8 bits for char
+        _data = self.logic(bits)
+        self.assign(_status, f"$fgets({_line}, {fd})", _allow_str=True)
+        self.assign(_status, f'$sscanf({_line}, "%x", {_data})', _allow_str=True)
+        return _data
 
     def _make_task_function(self, task_name):
         def task_func(*args):
